030f82328a4a1272eb834e352252b2d971dc4978
Duncan Morris <duncanmorris07@outlook.com>
2020-10-13 17:04:27 +0200

    #1 folder structure

---
+++ src/SuppliersServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+namespace duncanrmorris\suppliers;
+
+use Illuminate\Support\ServiceProvider;
+
+class SuppliersServiceProvider extends ServiceProvider
+
+{
+    
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/views','projectsmodule');
+        $this->loadMigrationsFrom(__DIR__.'/migrations');
+        
+    }
+
+    public function register()
+    {
+        
+    }
+}
+
